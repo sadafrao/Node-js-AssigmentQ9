cb87265bd7d73e5c952b4b93496de5a8403c08e2
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:27:02 -0800

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,5 @@
+//9. Favorite Number : store Your favorite number in a variable .
+//then, using that variable , create a message that reveals Your favorite number .Print that message.
+//Q9
+var num = 1989;
+console.log("My fvourate number is ".concat(num));
---
+++ Q9.ts
@@ -0,0 +1,7 @@
+//9. Favorite Number : store Your favorite number in a variable .
+//then, using that variable , create a message that reveals Your favorite number .Print that message.
+  
+//Q9
+let num: number = 1989
+
+console.log(`My fvourate number is ${num}`)
